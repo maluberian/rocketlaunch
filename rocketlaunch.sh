3f3bdaf7a6ad9fc941d1f48298e0994c7ea1282c
Dustin Clifford <dclifford@sitesoftllc.net>
2016-08-09 00:03:32 -0400

    updating scripts to make them a little smarter

--- rocketlaunch.sh
+++ rocketlaunch.sh
@@ -3,11 +3,8 @@
 GIT_REPO="https://github.com/maluberian/rocketlaunch.git"
 GIT_LOCAL_DIR="/tmp/rocketlaunch"
 
-PROFILE="/home/pi/.profile"
-PROFILE_TMP="/tmp/.profile.tmp"
-
-RC_FILE="/etc/rc.local"
-RC_TMP="/tmp/rc.tmp"
+# install default requirements
+sudo pip install scratchpy
 
 # clone repository
 if [[ -d $GIT_LOCAL_DIR ]]
@@ -19,9 +16,19 @@ git clone https://github.com/maluberian/rocketlaunch.git /tmp/rocketlaunch
 # copy files to the right spots
 cp /tmp/rocketlaunch/rocketlaunch.py /home/pi
 cp /tmp/rocketlaunch/rocketlaunch.sb /home/pi
+cp /tmp/rocketlaunch/uninstall.rocketlaunch.sh /home/pi/rocketlaunch/.uninstall.rocketlaunch.sh
+chown pi:pi /home/pi/rocketlaunch/.uninstall.rocketlaunch.sh
+cp /tmp/rocketlaunch/rocketlaunch.common.sh /home/pi/rocketlaunch/.rocketlaunch.common.sh
+chown pi:pi /home/pi/rocketlaunch/.rocketlaunch.common.sh
 chown pi:pi /home/pi/rocketlaunch.*
 
+source /home/pi/rocketlaunch/.rocketlaunch.common.sh
+
 # setup the rc.local
+if [[ ! -e $RC_SAVE ]]
+then
+	cp $RC_FILE $RC_SAVE
+fi
 cp $RC_FILE $RC_TMP
 
 sed -e "/^exit.*/Q" $RC_FILE > $RC_TMP
@@ -31,16 +38,26 @@ mv $RC_TMP $RC_FILE
 sed -e "s/.*rocketlaunch.*//" $RC_FILE > $RC_TMP
 mv $RC_TMP $RC_FILE
 
+echo -e "amixer cset numid=3 1" >> $RC_FILE
 echo -e "amixer -c 0 set PCM playback 100% unmute" >> $RC_FILE
-echo -e "/usr/bin/python /home/pi/rocketlaunch.py &" >> $RC_FILE
 echo >> $RC_FILE
 echo "exit 0" >> $RC_FILE
 
 # setup HOME profile to run 
+if [[ ! -e $PROFILE_SAVE ]]
+then
+	cp $PROFILE $PROFILE_SAVE
+fi
 sed -e "s/.*rocketlaunch.*//" $PROFILE > $PROFILE_TMP
 mv $PROFILE_TMP $PROFILE
-echo -e "/usr/bin/scratch /home/pi/rocketlaunch.sb &" >> $PROFILE
+echo -e "if [[ -e $LAUNCH_FILE ]]" >> $PROFILE
+echo -e "then" >> $PROFILE
+echo -e "\t/usr/bin/scratch /home/pi/rocketlaunch.sb && /usr/bin/python /home/pi/rocketlaunch.py" >> $PROFILE
+echo -e "\trm $$LAUNCH_FILE" >> $PROFILE
+echo -e "fi" >> $PROFILE
+echo  >> $PROFILE
+echo -e "alias runlaunch='touch $LAUNCH_FILE'" >> $PROFILE
+echo -e "alias cancellaunch='rm $LAUNCH_FILE'" >> $PROFILE
+chown pi:pi /home/pi/.profile
 
-
-
-
+rm -rf $GIT_LOCAL_DIR
---
+++ uninstall.rocketlaunch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /home/pi/.rocketlaunch.common.sh
+
