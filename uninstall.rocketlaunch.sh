1266e2aa9c2501f26580b72aa3201051af97d868
Dustin Clifford <dclifford@sitesoftllc.net>
2016-08-10 01:26:18 -0400

    removing old env file

--- uninstall.rocketlaunch.sh
+++ uninstall.rocketlaunch.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-source /home/pi/.rocketlaunch.common.sh
-
 mv /home/pi/.profile.save /home/pi/.profile
-
 rm /home/pi/.profile.normal /home/pi/.profile.launch /home/pi/rocketlaunch* /home/pi/.uninstall.rocketlaunch.sh
